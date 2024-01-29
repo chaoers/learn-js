48b4f459978b8bd1c88ae67e7b0c41b2a8778657
Bryant-surface <chaoers@bu.edu>
2024-01-29 16:16:10 -0500

    class activities

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -25,7 +25,12 @@ function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    tableNode.appendChild(createTRNode([col1])).appendChild(addEditButton());
   }
   document.getElementById("root").appendChild(tableNode);
+}
+
+function addEditButton(): {
+  let btnEdit = document.createElement("button");
+
 }
